2d5f9c5a1ebc43a74308c92bd236e88a30ca0a89
J Clark <cla19072@byui.edu>
2025-05-24 18:43:53 -0600

    Basic Calendar Set Up

---
+++ calendarCode/Calendar.vue
@@ -0,0 +1,24 @@
+<!-- const dayjs = require('dayjs')
+import dayjs from 'dayjs'
+dayjs().format() -->
+
+<!-- Calendar.vue-->
+
+<template>
+  <!-- Parent container for the calendar month-->
+  <div class="calendar-month">
+    <!-- The Calendar header-->
+    <div class="calendar-month-header" <!-- Month name -->
+      <CalendarDateIndicator />
+      <!-- pagination-->
+      <CalendarDateSelector />
+    </div>
+    <!-- Calendar grid header -->
+    <CalendarWeekdays />
+
+    <!-- Calendar grid -->
+    <ol class="days-grid">
+      <CalendarMonthDayItem />
+    </ol>
+  </div>
+</template>
