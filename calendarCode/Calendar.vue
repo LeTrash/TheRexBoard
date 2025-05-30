e52a5fe621d94d8af65c0c2ed95adf84c40e4d11
J Clark <cla19072@byui.edu>
2025-05-29 21:11:53 -0600

    Added Weekdays

--- calendarCode/Calendar.vue
+++ calendarCode/Calendar.vue
@@ -8,10 +8,17 @@ dayjs().format() -->
   <!-- Parent container for the calendar month-->
   <div class="calendar-month">
     <!-- The Calendar header-->
-    <div class="calendar-month-header" <!-- Month name -->
-      <CalendarDateIndicator />
+    <div class="calendar-month-header">
+      <CalendarDateIndicator
+        :selected-date="selectedDate"
+        class="calendar-month-header-selected-month"
+      />
       <!-- pagination-->
-      <CalendarDateSelector />
+      <CalendarDateSelector
+        :current-date="today"
+        :selected-date="selectedDate"
+        @dateSelected="selectDate"
+      />
     </div>
     <!-- Calendar grid header -->
     <CalendarWeekdays />
@@ -22,3 +29,29 @@ dayjs().format() -->
     </ol>
   </div>
 </template>
+
+<script>
+import dayjs from "dayjs";
+import CalendarDateIndicator from "./CalendarDateIndicator.vue";
+import CalendarDateSelector from "./CalendarDateSelector.vue";
+
+export default {
+  components: {
+    CalendarDateIndicator,
+    CalendarDateSelector,
+  },
+
+  data() {
+    return {
+      selectedDate: dayjs(),
+      today: dayjs().format("YYYY-MM-DD"),
+    };
+  },
+
+  methods: {
+    selectDate(newSelectedDate) {
+      this.selectedDate = newSelectedDate;
+    },
+  },
+};
+</script>
